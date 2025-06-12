d026e3633eea47106ee13a66f04acfe95516ea7b
Yeonu-Kim <ywk0524@snu.ac.kr>
2025-06-12 16:38:03 +0900

    🔥 불필요한 console.log 삭제

--- src/components/common/navigation.tsx
+++ src/components/common/navigation.tsx
@@ -6,7 +6,6 @@ import { PATH } from '@/entities/path';
 
 export const Navigation = () => {
   const path = usePathname();
-  console.log(path);
 
   return (
     <nav className="flex gap-4 justify-end items-center h-20 px-4 border-b">

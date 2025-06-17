327dec2168c3b4481b734f9726ae22fa6daae543
Yeonu-Kim <ywk0524@snu.ac.kr>
2025-06-17 14:38:44 +0900

    ✨ 동적 메타데이터 적용

--- src/app/(movies)/movies/[id]/page.tsx
+++ src/app/(movies)/movies/[id]/page.tsx
@@ -1,9 +1,40 @@
 import { Suspense } from 'react';
 import { MovieDetail } from '@/components/movies/movie-detail';
 import { VideoList } from '@/components/movies/video-list';
+import { apiGenerator } from '@/api';
+import type { Movie } from '@/entities/movie';
+import { PATH } from '@/entities/path';
+import { Metadata } from 'next';
 
 type Params = {
   id: string;
+};
+
+export const generateMetadata = async ({
+  params,
+}: {
+  params: Params;
+}): Promise<Metadata> => {
+  const { id } = await params;
+  const response = await apiGenerator<unknown, Movie>({
+    path: PATH.MOVIE_DETAIL({ movieId: id }),
+    method: 'GET',
+  });
+
+  if (response.type === 'error') {
+    return {
+      title: 'Movie Detail',
+      description: 'Welcome to the movie sites!',
+    };
+  }
+  const { title, overview, backdrop_path } = response.data;
+  return {
+    title,
+    description: overview,
+    openGraph: {
+      images: backdrop_path,
+    },
+  };
 };
 
 export default async function Page({ params }: { params: Promise<Params> }) {
